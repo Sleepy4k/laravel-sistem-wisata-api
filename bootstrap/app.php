8766c5e7fddbc86620342a2faa07ae214258b4bd
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2025-09-06 03:10:17 +0700

    chore(bootstrap): update app bootstraping

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,6 +24,8 @@ return Application::configure(basePath: dirname(__DIR__))
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
         ]);
+
+        $middleware->redirectUsersTo(fn () => route('access.denied'));
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
