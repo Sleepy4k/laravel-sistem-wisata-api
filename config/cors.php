958d0b7a8b07329f64b051335a7fd00248ecdc73
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2025-09-14 20:31:49 +0700

    chore(config): update rbac and add cors confiig

---
+++ config/cors.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
+    'paths' => [
+        'access-denied',
+        'login',
+        'logout',
+        'dashboard/*',
+        'sanctum/csrf-cookie'
+    ],
+
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+
+    'allowed_origins' => ['*'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [
+        'Cache-Control',
+        'Content-Language',
+        'Content-Type',
+        'Expires',
+        'Last-Modified',
+        'Pragma',
+    ],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+];
--- config/rbac.php
+++ config/rbac.php
@@ -13,6 +13,7 @@ return [
     'list' => [
         'roles' => [
             'admin',
+            'pemdes',
             'pokdarwis',
             'bumdes',
         ],
@@ -23,78 +24,33 @@ return [
 
             'manage.pokdarwis.business',
             'manage.bumdes.business',
-
-            // Pokdarwis Permissions
-            'pokdarwis.tourist_ticket.viewAny',
-            'pokdarwis.tourist_ticket.create',
-            'pokdarwis.tourist_ticket.update',
-            'pokdarwis.tourist_ticket.delete',
-
-            'pokdarwis.river_tubing.viewAny',
-            'pokdarwis.river_tubing.create',
-            'pokdarwis.river_tubing.update',
-            'pokdarwis.river_tubing.delete',
-
-            'pokdarwis.stall_rental.viewAny',
-            'pokdarwis.stall_rental.create',
-            'pokdarwis.stall_rental.update',
-            'pokdarwis.stall_rental.delete',
-
-            'pokdarwis.umkm_rental.viewAny',
-            'pokdarwis.umkm_rental.create',
-            'pokdarwis.umkm_rental.update',
-            'pokdarwis.umkm_rental.delete',
-
-            'pokdarwis.fish_feed.viewAny',
-            'pokdarwis.fish_feed.create',
-            'pokdarwis.fish_feed.update',
-            'pokdarwis.fish_feed.delete',
-
-            // Bumdes Permissions
-            'bumdes.restaurant_rental.viewAny',
-            'bumdes.restaurant_rental.create',
-            'bumdes.restaurant_rental.update',
-            'bumdes.restaurant_rental.delete',
-
-            'bumdes.internet_rental.viewAny',
-            'bumdes.internet_rental.create',
-            'bumdes.internet_rental.update',
-            'bumdes.internet_rental.delete',
+        ],
+        'dynamic_permissions' => [
+            'pokdarwis' => [
+                'tiket-wisata',
+                'river-tubing',
+                'sewa-warung',
+                'sewa-umkm',
+                'pakan-ikan ',
+            ],
+            'bumdes' => [
+                'sewa-resto',
+                'sewa-internet',
+            ],
         ],
     ],
 
     /* Permissions for each role */
     'permissions' => [
         'admin' => 'all',
+        'pemdes' => [
+            'view.pokdarwis.report',
+            'view.bumdes.report',
+        ],
         'pokdarwis' => [
             'view.pokdarwis.report',
 
             'manage.pokdarwis.business',
-
-            'pokdarwis.tourist_ticket.viewAny',
-            'pokdarwis.tourist_ticket.create',
-            'pokdarwis.tourist_ticket.update',
-            'pokdarwis.tourist_ticket.delete',
-
-            'pokdarwis.river_tubing.viewAny',
-            'pokdarwis.river_tubing.create',
-            'pokdarwis.river_tubing.update',
-            'pokdarwis.river_tubing.delete',
-
-            'pokdarwis.stall_rental.viewAny',
-            'pokdarwis.stall_rental.create',
-            'pokdarwis.stall_rental.update',
-            'pokdarwis.stall_rental.delete',
-
-            'pokdarwis.umkm_rental.viewAny',
-            'pokdarwis.umkm_rental.create',
-            'pokdarwis.umkm_rental.update',
-            'pokdarwis.umkm_rental.delete',
-
-            'pokdarwis.fish_feed.viewAny',
-            'pokdarwis.fish_feed.create',
-            'pokdarwis.fish_feed.update',
-            'pokdarwis.fish_feed.delete',
         ],
         'bumdes' => [
             'view.pokdarwis.report',
@@ -102,41 +58,6 @@ return [
 
             'manage.pokdarwis.business',
             'manage.bumdes.business',
-
-            'pokdarwis.tourist_ticket.viewAny',
-            'pokdarwis.tourist_ticket.create',
-            'pokdarwis.tourist_ticket.update',
-            'pokdarwis.tourist_ticket.delete',
-
-            'pokdarwis.river_tubing.viewAny',
-            'pokdarwis.river_tubing.create',
-            'pokdarwis.river_tubing.update',
-            'pokdarwis.river_tubing.delete',
-
-            'pokdarwis.stall_rental.viewAny',
-            'pokdarwis.stall_rental.create',
-            'pokdarwis.stall_rental.update',
-            'pokdarwis.stall_rental.delete',
-
-            'pokdarwis.umkm_rental.viewAny',
-            'pokdarwis.umkm_rental.create',
-            'pokdarwis.umkm_rental.update',
-            'pokdarwis.umkm_rental.delete',
-
-            'pokdarwis.fish_feed.viewAny',
-            'pokdarwis.fish_feed.create',
-            'pokdarwis.fish_feed.update',
-            'pokdarwis.fish_feed.delete',
-
-            'bumdes.restaurant_rental.viewAny',
-            'bumdes.restaurant_rental.create',
-            'bumdes.restaurant_rental.update',
-            'bumdes.restaurant_rental.delete',
-
-            'bumdes.internet_rental.viewAny',
-            'bumdes.internet_rental.create',
-            'bumdes.internet_rental.update',
-            'bumdes.internet_rental.delete',
         ],
     ],
 ];
